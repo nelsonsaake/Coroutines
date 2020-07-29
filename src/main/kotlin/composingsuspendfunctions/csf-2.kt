d402e2a6c993ba4bd53f279c14209df791d40216
unknown <nelsonsaakekofi@gmail.com>
2020-07-29 13:42:20 +0000

    commit csf 2

--- src/main/kotlin/composingsuspendfunctions/csf-2.kt
+++ src/main/kotlin/composingsuspendfunctions/csf-2.kt
@@ -1,14 +1,15 @@
-package csf1
+package csf2
 
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
 fun main() = runBlocking<Unit> {
     val time = measureTimeMillis {
-        val one = doSomethingUsefulOne()
-        val two = doSomethingUsefulTwo()
-        println("The answer is ${one + two}")
+        val one = async { doSomethingUsefulOne() }
+        val two = async { doSomethingUsefulTwo() }
+        println("The answer is ${one.await() + two.await()}")
     }
     println("Completed in $time ms")
 }
