7bcdd5014f4413770a8e282437a3227afb7ee912
unknown <nelsonsaakekofi@gmail.com>
2020-07-29 09:23:40 +0000

    commit canto 6

---
+++ src/main/kotlin/cancellationsandtimeouts/canto-6.kt
@@ -0,0 +1,14 @@
+package cancellationsandtimeouts
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+   withTimeout(1300L) {
+       repeat(1000) { i ->
+           println("I'm sleeping $i ...")
+           delay(500L)
+       }
+   }
+}
+
+
